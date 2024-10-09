a31811d3eb21541cc1e622a6665dbfec62041ee2
Luis Vera <101475330+LuisVera1@users.noreply.github.com>
2024-10-09 14:50:54 -0600

    Working on the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,22 +1,30 @@
+import { Facebook } from '../icons/facebook';
 import './footer.css'
 
 export const Footer = () => {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('validating email')
+  }
+
   return (
     <footer >
 
       <img className="footer__wave" src="./bg-footer-top-desktop.svg" alt="wave" />
 
+      {/* container */}
       <div className='footer-section'>
 
+        {/* column #1 */}
+        <div className='footer__firstColumn'>
 
-        <div>
-
-          <img src="./logo.svg" alt="huddle" />
+          <img className='footer__logo--white' src="./logo.svg" alt="huddle" />
 
           <p className='footer__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nulla quam, hendrerit lacinia
             vestibulum a, ultrices quis sem.</p>
 
-          <ul>
+          <ul className='footer__list'>
             <li>
               <img src="./icon-phone.svg" alt="phone" />
               <p>Phone: +1-543-123-4567</p>
@@ -28,35 +36,31 @@ export const Footer = () => {
             </li>
           </ul>
 
-          <div>
-            <img src="./facebook.svg" alt="facebook" />
+          <div className='footer__icons'>
+            {/* <img className='icons__fb' src="./facebook.svg" alt="facebook" /> */}
+            <Facebook  className="icon-fb"/>
+          
             <img src="./instagram.svg" alt="instagram" />
             <img src="./twitter.svg" alt="twitter" />
           </div>
         </div>
 
+        {/* column #2 */}
+        <div className='footer__secondColumn'>
 
-        <div>
-
-          <h3>Newsletter</h3>
-          <p>To recieve tips on how to grow your community, sign up to our weekly newsletter. We’ll never
+          <h3 className='footer__newsletterTitle'>Newsletter</h3>
+          <p className='footer__newsletterText'>To recieve tips on how to grow your community, sign up to our weekly newsletter. We’ll never
             send you spam or pass on your email address</p>
-          <button>Subscribe</button>
+
+          <form className='footer__form'>
+            <div>
+              <input className='footer__input' type="text" />
+              <p className='footer__input--message'>check your email please</p>
+            </div>
+            <button className='footer__btn' onClick={(e) => handleSubmit(e)}>Subscribe</button>
+          </form>
 
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
       </div>
     </footer>
   )
